831927cb64554539e5c34b94417d8370e80a2431
Veru44ia <vera3188@gmail.com>
2021-08-17 23:05:57 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  if (n > 0) {
+    let reversed = String(n).split("").reverse().join("");
+      return +reversed;
+  } else {
+    let reversed = String(n).split("").reverse().slice(0, -1).join("");
+      return +reversed;
+  }
 }
+
